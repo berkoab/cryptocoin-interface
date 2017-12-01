60867b23c8b9cad32b9f0851208585791360057b
berkoab <aryeh.berkowitz@gmail.com>
2017-12-01 16:07:53 +0000

    search params

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SearchParams } from '../searchParams'
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-  date2 = new Date("Thu Jan 01 2015 00:00:00 GMT-0500 (EST)");
-  gmtDate = '2015-01-01T00:00:00.000Z';
-  myVar='2015-01-01';
+  searchParams: SearchParams;
+
   currencies = ['USD', 'CHI'];
 
   constructor() { }
